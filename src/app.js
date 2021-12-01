e3a68b87db1f20bb637789b4bd4e7fba0b2c5770
Sergio Júnior <sergio.smmjr@gmail.com>
2021-12-01 18:40:57 -0300

    Renaming variables and creating racket

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 //Ball Variables
-let xCoordenate = 300;
-let yCoordenate = 200;
+let xBall = 300;
+let yBall = 200;
 let diameter = 20;
 let radius = diameter / 2;
 
 //Velocity Variables
 let xVelocityBall = 7;
 let yVelocityBall = 7;
+
+//Racket Variables
+let xRacket = 5;
+let yRacket = 150;
+let widthRacket = 10;
+let heightRacket = 90;
 
 function setup() {
     createCanvas(600, 400);
@@ -16,24 +22,29 @@ function draw() {
     background(0);
     showBall();
     movimentBall();
-    verifiedColision();    
+    verifiedColision();
+    showRacket();   
 }
 
 function showBall(){
-    circle(xCoordenate, yCoordenate, diameter);
+    circle(xBall, yBall, diameter);
 }
 
 function movimentBall(){
-    xCoordenate += xVelocityBall;
-    yCoordenate += yVelocityBall;
+    xBall += xVelocityBall;
+    yBall += yVelocityBall;
 }
 
 function verifiedColision(){
-    if(xCoordenate + radius > width || xCoordenate - radius < 0){
+    if(xBall + radius > width || xBall - radius < 0){
         xVelocityBall *= -1;
     }
 
-    if(yCoordenate + radius > height || yCoordenate - radius < 0){
+    if(yBall + radius > height || yBall - radius < 0){
         yVelocityBall *= -1;
     }
+}
+
+function showRacket(){
+    rect(xRacket, yRacket, widthRacket, heightRacket);
 }
